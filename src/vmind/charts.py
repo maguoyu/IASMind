4cbc6a117916d44b9da9c280a47ffa83910548ce
maguoyu <348408633@qq.com>
2025-07-23 15:17:33 +0800

    后台添加 vmind

---
+++ src/vmind/charts.py
@@ -0,0 +1,48 @@
+async def invoke_vmind(
+        self,
+        file_name: str,
+        output_type: str,
+        task_type: str,
+        insights_id: list[str] = None,
+        dict_data: list[dict[Hashable, Any]] = None,
+        chart_description: str = None,
+        language: str = "zh",
+    ):
+        llm_config = {
+            "base_url": self.llm.base_url,
+            "model": self.llm.model,
+            "api_key": self.llm.api_key,
+        }
+        vmind_params = {
+            "llm_config": llm_config,
+            "user_prompt": chart_description,
+            "dataset": dict_data,
+            "file_name": file_name,
+            "output_type": output_type,
+            "insights_id": insights_id,
+            "task_type": task_type,
+            "directory": str(config.workspace_root),
+            "language": language,
+        }
+        # build async sub process
+        process = await asyncio.create_subprocess_exec(
+            "npx",
+            "ts-node",
+            "src/vmind.ts",
+            vmind_params,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            cwd=os.path.dirname(__file__),
+        )
+        input_json = json.dumps(vmind_params, ensure_ascii=False).encode("utf-8")
+        try:
+            stdout, stderr = await process.communicate(input_json)
+            stdout_str = stdout.decode("utf-8")
+            stderr_str = stderr.decode("utf-8")
+            if process.returncode == 0:
+                return json.loads(stdout_str)
+            else:
+                return {"error": f"Node.js Error: {stderr_str}"}
+        except Exception as e:
+            return {"error": f"Subprocess Error: {str(e)}"}
